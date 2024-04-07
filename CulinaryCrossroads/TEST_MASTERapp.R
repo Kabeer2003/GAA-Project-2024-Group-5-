06dd0916a42b4d5ed6df3c821d0ec3e436f5ea5d
aidan445 <aidancheah.2021@scis.smu.edu.sg>
2024-04-07 23:10:19 +0800

    tinkering with module2
    kabeer pls see

--- CulinaryCrossroads/TEST_MASTERapp.R
+++ CulinaryCrossroads/TEST_MASTERapp.R
@@ -1,20 +1,22 @@
 library(shiny)
 
-source("TEST_MOD3.R")  # Ensure this file defines module3UI and module3 functions
+source("TEST_MOD3.R")
+source("TEST_MOD2.R")
 
 # Master App UI
 ui <- fluidPage(
   titlePanel("Master Shiny App for Hawker Centres"),
   tabsetPanel(
-    # Assuming the other module UIs are commented out for testing
-    tabPanel("Module 3", module3UI("module3")),  # Ensure module3UI is defined in TEST_MOD3.R or elsewhere
-    selected = "Module 3"  # Set the default tab to Module 3
+    tabPanel("Module 2", module2UI("module2")),
+    tabPanel("Module 3", module3UI("module3")),
+    selected = "Module 2"  # Set the default tab to Module 3
   )
 )
 
 # Master App Server
 server <- function(input, output, session) {
-  callModule(module3, "module3")  # Ensure the module3 function is defined and correctly sourced
+  callModule(module2, "module2")
+  callModule(module3, "module3")
 }
 
 # Run the App
